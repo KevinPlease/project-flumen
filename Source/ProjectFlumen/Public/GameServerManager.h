5e53bf61a8ccd03ee4474d3e3ccc88d44574f9cf
Kevin <kevin_beqo@hotmail.com>
2022-02-03 01:45:19 +0100

    - Fix for #143 , #152  etc.
    - Includes version change for android to 0.8.8
    - General improvements and input handling during gameplay transitions.

--- Source/ProjectFlumen/Public/GameServerManager.h
+++ Source/ProjectFlumen/Public/GameServerManager.h
@@ -2,12 +2,6 @@
 
 #include "CoreMinimal.h"
 #include "UObject/NoExportTypes.h"
-
-#if UE_SERVER
-#include "gsdk.h"
-#include <vector>
-#endif
-
 #include "GameServerManager.generated.h"
 
 /**
@@ -19,20 +13,7 @@ class PROJECTFLUMEN_API UGameServerManager : public UObject
 	GENERATED_BODY()
 
 public:
-	UFUNCTION(BlueprintPure, meta = (DisplayName = "Create Game Server Manager", HidePin = "WorldContextObject", DefaultToSelf = "WorldContextObject"), Category = "Game Server")
-	static UGameServerManager* CreateGameServerManager(UObject* WorldContextObject);
-
 	UFUNCTION(BlueprintCallable, Category = "Game Server")
-	void PlayerConnected(FString playfabId);
+	static void ShutdownServer(bool force = false);
 
-	UFUNCTION(BlueprintCallable, Category = "Game Server")
-	void PlayerDisconnected(FString playfabId);
-
-	UFUNCTION(BlueprintCallable, Category = "Game Server")
-	void ShutdownServer(bool force = false);
-
-private:
-	#if UE_SERVER
-	std::vector<Microsoft::Azure::Gaming::ConnectedPlayer> players;
-	#endif
 };
