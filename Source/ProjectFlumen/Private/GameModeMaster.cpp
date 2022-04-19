7eb16731f8edeae2179811b3dcf822b272779214
Kevin <kevin_beqo@hotmail.com>
2022-04-19 23:04:04 +0200

    [WIP] Issue #52 New base game mode override

--- Source/ProjectFlumen/Private/GameModeMaster.cpp
+++ Source/ProjectFlumen/Private/GameModeMaster.cpp
@@ -5,10 +5,10 @@
 
 void AGameModeMaster::PreLogin(const FString& Options, const FString& Address, const FUniqueNetIdRepl& UniqueId, FString& ErrorMessage)
 {
-	Super::PreLogin(options, Address, FUniqueNetIfRepl(), ErrorMessage);
+	Super::PreLogin(Options, Address, FUniqueNetIdRepl(), ErrorMessage);
 }
 
 APlayerController* AGameModeMaster::Login(UPlayer* NewPlayer, ENetRole InRemoteRole, const FString& Portal, const FString& Options, const FUniqueNetIdRepl& UniqueId, FString& ErrorMessage)
 {
-	return Super::Login(NewPlayer, InRemoteRole, Portal, Options, FUniqueNetIfRepl(), ErrorMessage);
+	return Super::Login(NewPlayer, InRemoteRole, Portal, Options, FUniqueNetIdRepl(), ErrorMessage);
 }
